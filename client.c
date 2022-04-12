851be2bc54397c02a0aee774b62dcc46cb76759e
Dorian-Gray73 <48696626+Dorian-Gray73@users.noreply.github.com>
2022-04-12 14:45:24 +0200

    feat parse_int : ajout de la parse_int + communication avec fonctionnel Co-authored-by: derguinm <derguinm@users.noreply.github.com>

--- client.c
+++ client.c
@@ -4,109 +4,132 @@
 #include <netdb.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
 #include "client.h"
+#include <ctype.h>
 
 
-//method parsing the input an expression mathematic from the user
-int parse_input(char *input, char *op1, char *op2, char *op)
+
+
+// method parsing the input an expression mathematic from the user : 2+2
+void parse_input(char *input, Request *req)
 {
-    int i = 0;
-    while(input[i] != '\0')
-    {
-        if(input[i] == '+' || input[i] == '-' || input[i] == '*' || input[i] == '/')
-        {
-            *op = input[i];
-            break;
-        }
+    
+    char* tmp=input;
+    char op;
+    char op1[10];
+    char op2[10];
+    int i=0;
+    size_t input_size=strlen(input);
+    //getting the first operand
+    while((i<input_size)&&(isdigit(tmp[i])!= 0)){
+        op1[i]=tmp[i];
         i++;
     }
-    if(i == 0)
-    {
-        return -1;
+    //getting the operator
+    op=tmp[i++];
+    int j=0;
+    //getting the second operand
+    while((i<input_size)&&(isdigit(tmp[i])!= 0)){
+        op2[j++]=tmp[i++];
     }
-    int j = 0;
-    while(input[i] != '\0')
-    {
-        if(input[i] == '+' || input[i] == '-' || input[i] == '*' || input[i] == '/')
-        {
-            break;
-        }
-        op1[j] = input[i];
-        i++;
-        j++;
-    }
-    op1[j] = '\0';
-    j = 0;
-    i++;
-    while(input[i] != '\0')
-    {
-        op2[j] = input[i];
-        i++;
-        j++;
-    }
-    op2[j] = '\0';
-    return 0;
-}
 
+    //setting the request 
+    int ope1=atoi(op1);
+    int ope2=atoi(op2);
+    req->params[0]=ope1;
+    req->params[1]=ope2;
+    if(op == '+'){
+        req->op=ADD;
+    }
+    else if(op == '-'){
+        req->op=SUB;
+    }
+    else if(op == '*'){
+        req->op=MUL;
+    }
+    else if(op == '/'){
+        req->op=DIV;
+    }
+    else{
+        printf("Invalid operation\n");
+        return;
+    }
+
+    //     char delim[] = op;
+    // char ** splited_input;
+    // char *token = strtok(input, delim);
+    // while (token != NULL)
+    // {
+    
+    //     token = strtok(NULL, delim);
+    //     req->params[1] = atoi(token);
+    // }
+    
+    
+        
+    //req->params[0] = atoi(token[0]);
+    //req->params[1] = atoi(token[1]);
+    
+
+
+}
 
 int main()
 {
-  struct sockaddr_in serverAddr;
-  int socketFd = socket(AF_INET, SOCK_STREAM, 0);
+    struct sockaddr_in serverAddr;
+    int socketFd = socket(AF_INET, SOCK_STREAM, 0);
 
-  bzero(&serverAddr, sizeof(serverAddr));
+    bzero(&serverAddr, sizeof(serverAddr));
 
-  serverAddr.sin_family = AF_INET;
-  serverAddr.sin_port = htons(22000);
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(22000);
 
-  bool initialization;
-  if (setsockopt(socketFd, SOL_SOCKET, SO_REUSEADDR, &initialization, sizeof(int)) == -1)
-  {
-    printf("Error during socket configuration!\n");
-    return -1;
-  }
+    bool initialization;
+    if (setsockopt(socketFd, SOL_SOCKET, SO_REUSEADDR, &initialization, sizeof(int)) == -1)
+    {
+        printf("Error during socket configuration!\n");
+        return -1;
+    }
 
-  // Set server address to localhost "127.0.0.1" 
-  inet_pton(AF_INET, "127.0.0.1", &(serverAddr.sin_addr));
+    // Set server address to localhost "127.0.0.1"
+    inet_pton(AF_INET, "127.0.0.1", &(serverAddr.sin_addr));
 
-  // Connect to the server
-  connect(socketFd, (struct sockaddr *)&serverAddr, sizeof(struct sockaddr_in));
+    // Connect to the server
+    connect(socketFd, (struct sockaddr *)&serverAddr, sizeof(struct sockaddr_in));
 
-  Request req = {
-      .ack = ACK,
-      .id = 0,
-      .op = MUL,
-      .params = {8, 2}
-  };
+    Request req = {
+        .ack = ACK,
+        .id = 0,
+    };
 
-  Response res = {0};
+    Response res = {0};
 
-  while (true) {
-    printRequest(req);
+    while (true)
+    {
+        // TODO replace with parsing  : faire l'appel a la fonction parse_input
+        char expression[10];
+        printf("Enter an expression (ex: 2+2): ");
+        scanf("%s", expression);
+        parse_input(expression, &req);
+        printRequest(req);
 
-    // Send a request to the server
-    (void)(write(socketFd, (char *)&req, sizeof(Request)));
+        // Send a request to the server
+        (void)(write(socketFd, (char *)&req, sizeof(Request)));
 
-    // Read server's response
-    (void)(read(socketFd, (char *)&res, sizeof(Response)));
+        // Read server's response
+        (void)(read(socketFd, (char *)&res, sizeof(Response)));
 
-    printResponse(res);
+        printResponse(res);
+        bzero(&res, sizeof(Response));
 
-    bzero(&res, sizeof(Response));
 
-    // TODO replace with parsing  : faire l'appel a la fonction parse_input
-    char op1[10];
-    char op2[10];
-    char op;
-    printf("Enter an expression (ex: 2+2): ");
-    scanf("%s", op1);
-    parse_input(op1, op1, op2, &op); 
-    getchar();
-  }
+        // getchar();
+    }
 
-  close(socketFd);
+    close(socketFd);
 
-  return 0;
+    return 0;
 }
--- client.h
+++ client.h
@@ -2,5 +2,6 @@
 #define _CLIENT_H_
 
 #include "common.h"
-
+int find_math_operation(char *op);
+void parse_input(char *input,Request* req);
 #endif
